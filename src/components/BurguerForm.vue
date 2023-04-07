9fe5be693045bfba4bbf3e6edd105afa0e8c0b98
gustavbarbosaa <gustavoaraujohab@gmail.com>
2023-04-07 20:30:27 -0300

    Crição do formulario de criação do burguer

---
+++ src/components/BurguerForm.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <p>Componente de mensagem</p>
+    <div>
+        <form id="burguer-form">
+            <div class="input-container">
+                <label for="nome">Nome do cliente</label>
+                <input type="text" name="nome" id="nome" v-model="nome" placeholder="Informe o seu nome">
+            </div>
+            <div class="input-container">
+                <label for="pao">Escolha o seu pão</label>
+                <select name="pao" id="pao" v-model="pao">
+                    <option value="">Selecione o seu pão</option>
+                    <option value="integral">Integral</option>
+                </select>
+            </div>
+            <div class="input-container">
+                <label for="carne">Escolha a sua carne</label>
+                <select name="carne" id="carne" v-model="carne">
+                    <option value="">Selecione a sua carne</option>
+                    <option value="costela">Costela</option>
+                </select>
+            </div>
+            <div class="input-container" id="opcionais-container">
+                <label id="opcionais-title" for="opcionais">Escolha os opcionais</label>
+                <div class="checkbox-container">
+                    <input type="checkbox" name="opcionais" id="opcionais" v-model="opcionais" value="salame">
+                    <span>Salame</span>
+                </div>
+            </div>
+            <div class="input-container">
+                <button class="submit-btn" type="submit">Criar o seu Burguer</button>
+            </div>
+        </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'BurguerForm'
+}
+</script>
+
+<style scoped>
+
+#burguer-form {
+    max-width: 400px;
+    margin: 0 auto;
+}
+
+.input-container {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+}
+
+label {
+    font-weight: bold;
+    margin-bottom: 15px;
+    color: #222;
+    padding: 5px 10px;
+    border-left: 4px solid #fcba03;
+}
+
+input, select {
+    padding: 5px 10px;
+    width: 300px;
+}
+
+#opcionais-container {
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+
+#opcionais-title {
+    width: 100%;
+}
+
+.checkbox-container {
+    display: flex;
+    align-items: flex-start;
+    width: 50%;
+    margin-bottom: 20px;
+}
+
+.checkbox-container span,
+.checkbox-container input {
+    width: auto;
+}
+
+.checkbox-container span {
+    margin-left: 6px;
+    font-weight: bold;
+}
+
+.submit-btn {
+    background-color: #222;
+    color: #fcba03;
+    font-weight: bold;
+    border: 2px solid #222;
+    padding: 10px;
+    font-size: 16px;
+    margin: 0 auto;
+    cursor: pointer;
+    transition: .5s;
+    border-radius: 5px;
+}
+
+.submit-btn:hover {
+    background-color: transparent;
+    color: #222;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,19 +2,21 @@
   <Banner />
   <div class="mainContainer">
     <h1>Monte o seu Burguer</h1>
-    <p>formulario</p>
+    <BurguerForm />
   </div>
 </template>
 
 <script>
 
 import Banner from '../components/Banner.vue';
+import BurguerForm from '../components/BurguerForm.vue';
 
 export default {
 
   name: 'Home',
   components: {
     Banner,
+    BurguerForm,
   }
 }
 
