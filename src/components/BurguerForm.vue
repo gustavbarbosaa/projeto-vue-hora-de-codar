5c15544397d7ffca0e4b7f168e6dff474dd1817c
gustavbarbosaa <gustavoaraujohab@gmail.com>
2023-04-08 20:32:37 -0300

    inserção de dados no sistema

--- src/components/BurguerForm.vue
+++ src/components/BurguerForm.vue
@@ -2,7 +2,7 @@
   <div>
     <p>Componente de mensagem</p>
     <div>
-        <form id="burguer-form">
+        <form id="burguer-form" @submit="createBurguer">
             <div class="input-container">
                 <label for="nome">Nome do cliente</label>
                 <input type="text" name="nome" id="nome" v-model="nome" placeholder="Informe o seu nome">
@@ -65,7 +65,6 @@ export default {
             pao: null,
             carne: null,
             opcionais: [],
-            status: 'Solicitado',
             msg: null,
         }
     },
@@ -77,6 +76,34 @@ export default {
             this.paes = data.paes;
             this.carnes = data.carnes;
             this.opcionaisdata = data.opcionais;
+        },
+        async createBurguer(e) {
+            e.preventDefault();
+
+            const data = {
+                nome: this.nome,
+                carne: this.carne,
+                pao: this.pao,
+                opcionais: Array.from(this.opcionais),
+                status: 'Solicitado',
+            }
+
+            const json = JSON.stringify(data);
+
+            const req = await fetch('http://localhost:3000/burgers', {
+                method: 'POST',
+                headers: {"Content-Type": "application/json"},
+                body: json,
+            });
+
+            const res = await req.json();
+
+            this.nome = '';
+            this.pao = '';
+            this.carne = '';
+            this.opcionais = '';
+
+            console.log(res);
         }
     },
     mounted() {
